4e41db9502108b408af57fae22b109e5a544f550
srilakshmi9966 <srilakshmiariveni164@gmail.com>
2026-06-18 17:52:13 +0530

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,415 +1,98 @@
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<url>
-<loc>https://teksacademy.com/</loc>
-<lastmod>2026-06-18T06:20:08+00:00</lastmod>
-<changefreq>daily</changefreq>
-<priority>1.00</priority>
-</url>
+// app/sitemap.js
 
-<url>
-  <loc>https://teksacademy.com/course</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
+export default function sitemap() {
+  const baseUrl = "https://teksacademy.com";
 
-<url>
-  <loc>https://teksacademy.com/courses/best-full-stack-python-development-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
+  const routes = [
+    "/",
+    "/course",
 
-<url>
-  <loc>https://teksacademy.com/courses/best-full-stack-java-development-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
+    // Courses
+    "/courses/best-full-stack-python-development-course-training-institute",
+    "/courses/best-full-stack-java-development-course-training-institute",
+    "/courses/best-data-science-course-training-institute",
+    "/courses/best-awsplusdevops-course-training-institute",
+    "/courses/best-digital-marketing-course-training-institute",
+    "/courses/best-data-analytics-course-training-institute",
+    "/courses/best-multimedia-course-training-institute",
+    "/courses/best-bim-building-information-modeling-course-training-institute",
+    "/courses/best-sap-fico-finance-and-controlling-course-training-institute",
+    "/courses/best-medical-coding-course-training-institute",
+    "/courses/best-testingtools-course-training-institute",
+    "/courses/best-advance-excel-course-training-institute",
+    "/courses/best-autocad-course-training-institute",
+    "/courses/best-revit-mep-course-training-institute",
+    "/courses/best-business-analytics-course-training-institute",
+    "/courses/best-generative-ai-course-training-institute",
+    "/courses/best-sapmm-course-training-institute",
+    "/courses/best-cyber-security-course-training-institute",
 
-<url>
-  <loc>https://teksacademy.com/courses/best-data-science-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
+    // Branches
+    "/branch/best-software-training-institute-ameerpet",
+    "/branch/best-software-training-institute-hiteccity",
+    "/branch/best-software-training-institute-secunderabad",
+    "/branch/best-software-training-institute-dilsukhnagar",
+    "/branch/best-software-training-institute-mehdipatnam",
+    "/branch/best-software-training-institute-kukatpally",
+    "/branch/best-software-training-institute-bangalore",
+    "/branch/best-software-training-institute-visakhapatnam",
+    "/branch/best-software-training-institute-kompally",
 
-<url>
-  <loc>https://teksacademy.com/courses/best-awsplusdevops-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
+    // Success Stories
+    "/success-stories",
 
-<url>
-  <loc>https://teksacademy.com/courses/best-digital-marketing-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
+    // Resources
+    "/resources/ebook",
+    "/resources/interview-questions",
 
-<url>
-  <loc>https://teksacademy.com/courses/best-data-analytics-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
+    // Interview Questions
+    "/interview-questions/python-interview-questions",
+    "/interview-questions/java-interview-questions",
+    "/interview-questions/data-science-interview-questions",
+    "/interview-questions/digital-marketing-interview-questions",
+    "/interview-questions/aws-dev-interview-questions",
+    "/interview-questions/bim-interview-questions",
+    "/interview-questions/software-testing-interview-questions",
+    "/interview-questions/sap-interview-questions",
+    "/interview-questions/multimedia-interview-questions",
+    "/interview-questions/medical-coding-interview-questions",
+    "/interview-questions/advanced-excel-interview-questions",
+    "/interview-questions/auto-cad-interview-questions",
+    "/interview-questions/revit-interview-questions",
+    "/interview-questions/data-analytics-interview-questions",
+    "/interview-questions/sap-mm-interview-questions",
+    "/interview-questions/cyber-security-interview-questions",
+    "/interview-questions/generative-ai-interview-questions",
+    "/interview-questions/business-analytics-interview-questions",
 
-<url>
-  <loc>https://teksacademy.com/courses/best-multimedia-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
+    // Discover
+    "/discover/about-us",
+    "/discover/gallery",
+    "/discover/media",
+    "/discover/contact-us",
+    "/discover/support",
 
-<url>
-  <loc>https://teksacademy.com/courses/best-bim-building-information-modeling-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
+    // Placements
+    "/placements/alumni",
+    "/placements/recruiters",
 
-<url>
-  <loc>https://teksacademy.com/courses/best-sap-fico-finance-and-controlling-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
+    // Others
+    "/apply-for-jobs",
+    "/blogs",
+  ];
 
-<url>
-  <loc>https://teksacademy.com/courses/best-medical-coding-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/courses/best-testingtools-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/courses/best-advance-excel-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/courses/best-autocad-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/courses/best-revit-mep-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/courses/best-business-analytics-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/courses/best-generative-ai-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/courses/best-sapmm-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/courses/best-cyber-security-course-training-institute</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.9</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/branch/best-software-training-institute-ameerpet</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/branch/best-software-training-institute-hiteccity</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/branch/best-software-training-institute-secunderabad</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/branch/best-software-training-institute-dilsukhnagar</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/branch/best-software-training-institute-mehdipatnam</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/branch/best-software-training-institute-kukatpally</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/branch/best-software-training-institute-bangalore</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/branch/best-software-training-institute-visakhapatnam</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/branch/best-software-training-institute-kompally</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/success-stories</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/resources/ebook</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/resources/interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/python-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/java-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/data-science-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/digital-marketing-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/aws-dev-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/bim-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/software-testing-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/sap-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/multimedia-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/medical-coding-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/advanced-excel-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/auto-cad-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/revit-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/data-analytics-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/sap-mm-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/cyber-security-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/generative-ai-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/interview-questions/business-analytics-interview-questions</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/discover/about-us</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/discover/gallery</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/discover/media</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/discover/contact-us</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/discover/support</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/placements/alumni</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/placements/recruiters</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/apply-for-jobs</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
-
-<url>
-  <loc>https://teksacademy.com/blogs</loc>
-  <lastmod>2026-06-18T06:20:08+00:00</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>
-</urlset> 
- 
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency:
+      route === "/" || route === "/blogs"
+        ? "daily"
+        : "weekly",
+    priority:
+      route === "/"
+        ? 1
+        : route.startsWith("/courses")
+        ? 0.9
+        : 0.8,
+  }));
+}
